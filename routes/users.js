98f729687775c6f382fed8d597259c81dbc9ecb2
joker <jocker.ss.cat@gmail.com>
2024-05-15 13:18:20 +0800

    完成: LV1 註冊/登入/密碼重設/更新個資/取得個資

--- routes/users.js
+++ routes/users.js
@@ -14,16 +14,16 @@ const userRouter = express.Router();
 // ));
 
 // 註冊
-userRouter.get("/sign_up", handErrorAsync(async (req, res, next) => {
+userRouter.post("/sign_up", handErrorAsync(async (req, res, next) => {
   let { name, email, password, confirmPassword } = req.body;
-  if (name || email || password || confirmPassword) return next(appError(400, "請確實填寫資訊", next));
+  if (!(name && email && password && confirmPassword)) return next(appError(400, "請確實填寫資訊", next));
   if (password.trim() !== confirmPassword.trim()) return next(appError(400, "密碼不一致！", next));
   const validateName = validator.isEmpty(name.trim());
   if (validateName) return next(appError(400, "名稱未填寫"));
   const validateEmail = validator.isEmail(email.trim());
   if (!validateEmail) return next(appError(400, "信箱格式錯誤"));
-  const validatePsd = validator.isByteLength(password.trim(), { min: 6, max: 10 });
-  if (!validatePsd) return next(appError(400, "密碼需介於 6-10 字元之間"));
+  const validatePsd = validator.isByteLength(password.trim(), { min: 6, max: 18 });
+  if (!validatePsd) return next(appError(400, "密碼需介於 6-18 字元之間"));
 
   //信箱是否註冊過
   const user = await User.findOne({ email });
@@ -48,6 +48,7 @@ userRouter.post("/sign_in", handErrorAsync(async (req, res, next) => {
 
   // password原本為隱藏，透過select('+password')顯示取得
   const user = await User.findOne({ email }).select('+password');
+
   // 輸入密碼與雜湊密碼比對
   const auth = await bcrypt.compare(password, user.password);
   if (!auth) return next(appError(400, "密碼錯誤"));
@@ -56,27 +57,54 @@ userRouter.post("/sign_in", handErrorAsync(async (req, res, next) => {
 }
 ));
 
+//找回密碼
 userRouter.post("/updatePassword", isAuth, handErrorAsync(async (req, res, next) => {
-  let { email, password } = req.body;
-  const validateEmail = validator.isEmpty(email.trim());
-  if (validateEmail) return next(appError(400, "郵件未填寫"));
-  const validatePsd = validator.isEmail(password.trim());
-  if (validatePsd) return next(appError(400, "密碼未填寫"));
 
-  // password原本為隱藏，透過select('+password')顯示取得
-  const user = await User.findOne({ email }).select('+password');
-  // 輸入密碼與雜湊密碼比對
-  const auth = await bcrypt.compare(password, user.password);
-  if (!auth) return next(appError(400, "密碼錯誤"));
+  //再次檢查是否有登入，即將更改密碼
+  if (req.user === undefined) return next(appError(401, '你尚未登入！', next));
 
-  sendJWT(user, 201, res);
+  let newPassword = null;
+  const { password, confirmPassword } = req.body;
+
+  if (password.trim() !== confirmPassword.trim()) return next(appError(400, "密碼不一致！", next));
+  if (!validator.isByteLength(password.trim(), { min: 6, max: 18 })) return next(appError(400, "密碼需介於 6-18 字元之間"));
+
+  //加密密碼
+  newPassword = await bcrypt.hash(password, 12);
+  //更新密碼
+  const user = await User.findByIdAndUpdate(req.user.id, {
+    password: newPassword
+  });
+
+  sendJWT(user, 200, res);
 }
 ));
 
-// POST：{url}/users/sign_up：註冊
-// POST：{url}/users/sign_in：登入
-// POST：{url}/users/updatePassword: 重設密碼
-// GET：{url}/users/profile: 取得個人資料，需設計 isAuth middleware。
-// PATCH：{url}/users/profile: 更新個人資料，需設計 isAuth middleware
+//找回密碼
+userRouter.get("/profile", isAuth, handErrorAsync(async (req, res, next) => {
+  console.log(123);
+  //再次檢查是否有登入，即將更改密碼
+  if (req.user === undefined) return next(appError(401, '你尚未登入！', next));
+  const user = await User.findById(req.user.id);
+  res.status(201).json({
+    status: 'success',
+    user
+  });
+}
+));
+
+
+//更新個人資料
+userRouter.patch("/profile", isAuth, handErrorAsync(async (req, res, next) => {
+
+  if (req.user === undefined) return next(appError(401, '你尚未登入！'));
+  //檢查是否有修改密碼到
+  const updateKeys = Object.keys(req.body);
+  if (updateKeys.includes('password')) return next(appError(400, '只允許修改基本個資'));
+
+  const user = await User.findByIdAndUpdate(req.user.id, req.body);
+  sendJWT(user, 200, res);
+}
+));
 
 module.exports = userRouter;
